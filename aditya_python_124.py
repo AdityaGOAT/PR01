186faf04aa68b1cd2827fa6e2feeb6ded3cee2bf
aditya <adityaanair.bscibm2024@iar.ac.in>
2025-02-06 15:40:26 +0530

    python file for practical

---
+++ aditya_python_124.py
@@ -0,0 +1,18 @@
+print("hello, world!")
+
+a=13
+b=67
+c=78
+
+maximum=max(a,b,c)
+print("the maximum number is",maximum)
+
+a=7
+b=9
+result=a//b
+print(result)
+
+a=7
+b=9
+result=a%b
+print(result)
